21be1ac262a01afff5ec09a2d8765bba1bc28df0
aloxanh <ngochung77@gmail.com>
2023-06-16 23:22:23 +0700

    zalo login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-
+use Zalo\Zalo;
+use Illuminate\Support\Str;
+use Wilkques\PKCE\Generator;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,7 +21,56 @@ Auth::routes(['verify' => true]);
 Route::get('/', function () {
   return view('welcome');
 });
+Route::get('/calllback', function ($state) {
+    dd("calllback");
+});
+Route::get('/zalo', function () {
+      $config = array(
+        'app_id' => '270349389225145785',
+        'app_secret' => 'PtCJyx1eNY4Pr6W467Xu'
+    );
+    $zalo = new Zalo($config);
+    $helper = $zalo -> getRedirectLoginHelper();
+    $callbackUrl = "https://shipdemo.thienlong.vn/login/zalo/callback";
 
+    $codeVerifier =   Str::random(43) ;//bin2hex(random_bytes(32));
+       $codeChallenge = hash('sha256', $codeVerifier, true);
+       $state =  Str::random(40);
+
+
+      
+$codeVerifier = Generator::codeVerifier();
+
+$codeChallenge = Generator::codeChallenge($codeVerifier);
+
+       $verifierBytes = random_bytes(64);
+       $codeVerifier = rtrim(strtr(base64_encode($verifierBytes), "+/", "-_"), "=");
+      // dd($codeChallenge);
+       // Very important, "raw_output" must be set to true or the challenge
+       // will not match the verifier.
+     //  $challengeBytes = hash("sha256", $codeVerifier, true);
+      // $codeChallenge = rtrim(strtr(base64_encode($challengeBytes), "+/", "-_"), "=");
+   
+       // State token, a uuid is fine here
+       $state = uniqid();
+
+
+
+    //dd($codeChallenge . " - ".$state);
+   // Log::info("test call back");
+    $loginUrl = $helper->getLoginUrl($callbackUrl, $codeChallenge, $state);
+
+   return redirect($loginUrl);
+    
+
+    //$codeVerifier = PKCEUtil::genCodeVerifier();
+    //$codeChallenge = PKCEUtil::genCodeChallenge($codeVerifier);
+    //$codeVerifier = "your code verifier";
+    $zaloToken = $helper->getZaloToken($codeVerifier); // get zalo token
+   
+    $accessToken = $zaloToken->getAccessToken();
+    dd($accessToken);
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
