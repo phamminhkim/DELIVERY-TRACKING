3214f5ea2c9b633b5b3969041864f98f7daafe54
aloxanh <ngochung77@gmail.com>
2023-06-20 17:01:10 +0700

    Login app zalo ok

--- app/Http/Controllers/Api/Auth/ZaloController.php
+++ app/Http/Controllers/Api/Auth/ZaloController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\BaseController\ResponeseController;
 use App\Http\Controllers\Controller;
 use App\Models\SocialAccount;
+use App\Services\SocialAccountService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Zalo\Zalo;
+use Zalo\ZaloEndPoint;
 
 class ZaloController extends ResponeseController
 {
@@ -39,6 +43,35 @@ class ZaloController extends ResponeseController
      */
     public function login(Request $request)
     {
-        dd("login");
+        $accessTokenZalo = $request->access_token;
+        $config = array(
+            'app_id' => config("services.zalo.client_id") ,
+            'app_secret' =>  config("services.zalo.client_secret")
+        );
+
+        $zalo = new Zalo($config);
+    
+
+        $helper = $zalo->getRedirectLoginHelper();
+
+        $params = ['fields' => 'id,name,picture'];
+        $response = $zalo->get(ZaloEndPoint::API_GRAPH_ME, $accessTokenZalo, $params);
+        $result = $response->getDecodedBody(); // result
+        $service = new SocialAccountService;
+        $user = $service->createOrGetUserFromZalo($result);
+        
+        Auth::login($user);
+        $userLogin = Auth::user();
+        $accessToken = $userLogin->createToken('authToken')->accessToken;
+        $res['access_token'] =$accessToken;
+        if($user  ){
+           if($user->active == 1){
+            return $this->sendResponse( $res,'Logged in successfully');
+           }else{
+            return $this->sendFailedWithMessage( 'User is locked');
+           }
+        }else{
+            return $this->sendFailedWithMessage('Login is fault');
+        }
     }
 }
