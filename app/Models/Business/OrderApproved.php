d500c9bc7b770edbb968753d18fbc33140e40660
Nhật Trường <nhattruongtotran@gmail.com>
2023-06-20 16:44:52 +0700

    Cập nhật model (Order + OrderApproved)

---
+++ app/Models/Business/OrderApproved.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Business;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderApproved extends Model
+{
+    protected $fillable = [
+        'sap_so_coordinate_approval_date', 'sap_so_finance_approval_date'
+    ];
+    protected $casts = [
+        'sap_so_coordinate_approval_date' => 'datetime',
+        'sap_so_finance_approval_date' => 'datetime'
+    ];
+    protected $hidden = [
+        'order_id'
+    ];
+}
