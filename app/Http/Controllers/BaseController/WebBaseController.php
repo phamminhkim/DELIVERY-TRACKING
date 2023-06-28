64ccf82109019f62b99e4f5cd879de4eb7713a8e
aloxanh <ngochung77@gmail.com>
2023-06-28 14:49:47 +0700

    gán token

---
+++ app/Http/Controllers/BaseController/WebBaseController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\BaseController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class WebBaseController  extends ResponseController
+{
+    public function __construct()
+    {
+ 
+        $this->middleware('auth');
+
+        $this->middleware(function ($request, $next) {
+            //load menu từ Role_user
+            $auth_user = Auth()->user();
+            dd("dd");
+            $access_token = "";
+            // Nếu không phải login từ API thì tạo token cho user
+            if (!$auth_user->token()) {
+                if ($request->session() && $request->session()->get('user')) {
+                    $access_token = $request->session()->get('user');
+                } else {
+                    $authToken =  $auth_user->createToken('authToken');
+                    $access_token = $authToken->accessToken; // $auth_user->createToken('authToken')->accessToken;
+                    $auth_user->withAccessToken($access_token);
+                    $request->session()->put('user', $access_token);
+                }
+            }
+             
+            return $next($request);
+        });
+    }
+    public function sendResponse($result, $message = '', $code = 200)
+    {
+
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, $code);
+    }
+    public function sendError($error, $errorMessage = [], $code = 400)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error
+        ];
+        if (!empty($errorMessage)) {
+            $response['errors'] = $errorMessage;
+        }
+        return response()->json($response, $code);
+    }
+
+    public function sendOk()
+    {
+        return response();
+    }
+    public function sendSuccess($message)
+    {
+        $response = [
+            'success' => true,
+            'message' => $message
+        ];
+        return response()->json($response, 200);
+    }
+    public function sendFailedWithMessage($message)
+    {
+        $response = [
+            'success' => false,
+            'message' => $message
+        ];
+        return response()->json($response, 200);
+    }
+    public function sendFailedWithStatusCode($message, $code)
+    {
+        $response = [
+            'success' => false,
+            'message' => $message
+        ];
+        return response()->json($response, $code);
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\BaseController\WebBaseController;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class HomeController extends WebBaseController
 {
     /**
      * Create a new controller instance.
@@ -13,7 +14,28 @@ class HomeController extends Controller
      */
     public function __construct()
     {
+       
         $this->middleware('auth');
+
+        $this->middleware(function ($request, $next) {
+            //load menu từ Role_user
+            $auth_user = Auth()->user();
+
+            $access_token = "";
+            // Nếu không phải login từ API thì tạo token cho user
+            if (!$auth_user->token()) {
+                if ($request->session() && $request->session()->get('user')) {
+                    $access_token = $request->session()->get('user');
+                } else {
+                    $authToken =  $auth_user->createToken('authToken');
+                    $access_token = $authToken->accessToken; // $auth_user->createToken('authToken')->accessToken;
+                    $auth_user->withAccessToken($access_token);
+                    $request->session()->put('user', $access_token);
+                }
+            }
+           
+            return $next($request);
+        });
     }
 
     /**
