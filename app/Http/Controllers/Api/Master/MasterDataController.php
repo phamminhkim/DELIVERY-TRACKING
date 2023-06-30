1aede1bc844b7d346f1aa34ed83d47c2f8deef52
NguyenThu0110 <91600113+nguyenthu753951@users.noreply.github.com>
2023-06-30 16:59:32 +0700

    chưa xong MasterDataController

--- app/Http/Controllers/Api/Master/MasterDataController.php
+++ app/Http/Controllers/Api/Master/MasterDataController.php
@@ -11,33 +11,30 @@ class MasterDataController extends ResponseController
     public function sysCategory(Request $request, $category)
     {
         switch ($category) {
-                case '1':
-                    $handler = MasterRepository::customerRequest($request);
-                    $customers = $handler->getAvailableCustomers();
-        
-                    // Kiểm tra xem $customers có phải là một mảng hay không
-                    if (!is_array($customers)) {
-                        $customers = $customers->toArray();
-                    }
-        
-                    $customer = $handler->createNewCustomer();
-                    $customer = $handler->updateExistingCustomer();
-        
-                    //lấy ra các thuộc tính cần lấy
-                    $filteredCustomers = array_map(function ($customer) {
-                        return [
-                            'code' => $customer['code'],
-                            'name' => $customer['name'],
-                            'email' => $customer['email'],
-                            'phone_number' => $customer['phone_number'],
-                            'address' => $customer['address'],
-                        ];
-                    }, $customers);
-                    return $this->responseSuccess($filteredCustomers);
-                    break;            
-                default:
-                    return $this->responseError($handler->getMessage('Invalid category'), $handler->getErrors());
-                    break;                       
+            case '1':
+                $handler = MasterRepository::customerRequest($request);
+                $customers = $handler->getAvailableCustomers();
+
+                // Kiểm tra xem $customers có phải là một mảng hay không
+                if (!is_array($customers)) {
+                    $customers = $customers->toArray();
+                }
+
+                $customer = $handler->createNewCustomer();
+                $customer = $handler->updateExistingCustomer();
+
+                //lấy ra các thuộc tính cần lấy
+                $filteredCustomers = array_map(function ($customer) {
+                    return [
+                        'code' => $customer['code'],
+                        'name' => $customer['name'],
+                        'email' => $customer['email'],
+                        'phone_number' => $customer['phone_number'],
+                        'address' => $customer['address'],
+                    ];
+                }, $customers);
+                return $this->responseSuccess($filteredCustomers);
+                break; // Thêm break vào đây
             case '2':
                 $handler = MasterRepository::distributionChannelRequest($request);
                 $distributionChannels = $handler->getAvailableDistributionChannels();
