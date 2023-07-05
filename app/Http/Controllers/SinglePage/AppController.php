376d1b620b2702c5ef11dbf2355fa61bd5164c13
aloxanh <ngochung77@gmail.com>
2023-07-05 10:40:06 +0700

    truyền token cho singlepage

--- app/Http/Controllers/SinglePage/AppController.php
+++ app/Http/Controllers/SinglePage/AppController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\SinglePage;
 
+use App\Http\Controllers\BaseController\WebBaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AppController extends Controller
+class AppController extends WebBaseController
 {
     public function index(){
         return view("app.index");
