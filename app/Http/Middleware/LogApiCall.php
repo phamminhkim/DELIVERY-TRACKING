dbe9dfad464c44b58de364a3f5bd2f3e9984f074
Nhật Trường <nhattruongtotran@gmail.com>
2023-07-05 17:16:36 +0700

    Thêm middleware log api call

---
+++ app/Http/Middleware/LogApiCall.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class LogApiCall
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response->getStatusCode() != 200) {
+            Log::error('API Call Error:', [
+                'url' => $request->fullUrl(),
+                'method' => $request->method(),
+                'params' => $request->all(),
+                'ip' => $request->ip(),
+                'user_agent' => $request->userAgent(),
+                'response' => $response->getContent(),
+            ]);
+        }
+
+        return $response;
+    }
+}
