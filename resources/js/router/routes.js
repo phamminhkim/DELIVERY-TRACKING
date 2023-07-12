8d8d2c66e82ba0eb44df00722e52452459883716
NguyenThu0110 <91600113+nguyenthu753951@users.noreply.github.com>
2023-07-12 11:24:57 +0700

    sửa route.js

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -5,9 +5,9 @@ import DeliveryCustomer from '../components/home/DeliveryCustomer.vue'
 const routes = [
     { path: '/', component: MainScreen, name: 'MainScreen' },
     { path: '/home', component: MainScreen, name: 'MainScreen' },
-    // { path: '/delivery-partner', component: DeliveryPartners, name: 'DeliveryPartners' },
-    // { path: '/delivery-user', component: DeliveryUser, name: 'DeliveryUser' },
-    // { path: '/delivery-customer', component: DeliveryCustomer, name: 'DeliveryCustomer' },
+    { path: '/delivery-partner', component: DeliveryPartners, name: 'DeliveryPartners' },
+    { path: '/delivery-user', component: DeliveryUser, name: 'DeliveryUser' },
+    { path: '/delivery-customer', component: DeliveryCustomer, name: 'DeliveryCustomer' },
 
   ];
 
