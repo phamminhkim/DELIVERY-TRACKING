11cee2b4b281dc4299dc3cdc4f528e3c8966241f
Nhật Trường <nhattruongtotran@gmail.com>
2023-07-13 09:46:19 +0700

    Cập nhật lại tên components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,39 +1,54 @@
-require('./bootstrap');
-import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+require("./bootstrap");
+import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 import router from "./router";
 import VueRouter from "vue-router";
-import moment from 'moment';
+import moment from "moment";
 
-Vue.filter('formatDate', function(value) {
+Vue.filter("formatDate", function (value) {
     if (value) {
-        return moment(String(value)).format('DD/MM/YYYY')
+        return moment(String(value)).format("DD/MM/YYYY");
     }
 });
 Vue.use(VueRouter);
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('home', require('./components/home/MainScreen.vue').default);
-Vue.component('delivery-partner', require('./components/home/DeliveryPartners.vue').default);
-Vue.component('delivery-user', require('./components/home/DeliveryUser.vue').default);
-Vue.component('delivery-customer', require('./components/home/DeliveryCustomer.vue').default);
-Vue.component('admin-container', require('./components/AdminContainer.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.component("home", require("./components/home/MainScreen.vue").default);
+Vue.component(
+    "delivery-partner",
+    require("./components/home/DeliveryPartners.vue").default
+);
+Vue.component(
+    "master-users",
+    require("./components/home/MasterUsers.vue").default
+);
+Vue.component(
+    "delivery-customer",
+    require("./components/home/DeliveryCustomer.vue").default
+);
+Vue.component(
+    "admin-container",
+    require("./components/AdminContainer.vue").default
+);
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     data() {
-        return {}
+        return {};
     },
     router,
 });
--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,14 +1,21 @@
-import MainScreen from '../components/home/MainScreen.vue'
-import DeliveryPartners from '../components/home/DeliveryPartners.vue'
-import DeliveryUser from '../components/home/DeliveryUser.vue'
-import DeliveryCustomer from '../components/home/DeliveryCustomer.vue'
+import MainScreen from "../components/home/MainScreen.vue";
+import DeliveryPartners from "../components/home/DeliveryPartners.vue";
+import MasterUsers from "../components/home/MasterUsers.vue";
+import DeliveryCustomer from "../components/home/DeliveryCustomer.vue";
 const routes = [
-    { path: '/', component: MainScreen, name: 'MainScreen' },
-    { path: '/home', component: MainScreen, name: 'MainScreen' },
-    { path: '/delivery-partner', component: DeliveryPartners, name: 'DeliveryPartners' },
-    { path: '/delivery-user', component: DeliveryUser, name: 'DeliveryUser' },
-    { path: '/delivery-customer', component: DeliveryCustomer, name: 'DeliveryCustomer' },
-
+    { path: "/", component: MainScreen, name: "MainScreen" },
+    { path: "/home", component: MainScreen, name: "MainScreen" },
+    {
+        path: "/delivery-partner",
+        component: DeliveryPartners,
+        name: "DeliveryPartners",
+    },
+    { path: "/master-users", component: MasterUsers, name: "MasterUsers" },
+    {
+        path: "/delivery-customer",
+        component: DeliveryCustomer,
+        name: "DeliveryCustomer",
+    },
 ];
 
 export default routes;
