4f73536647e999926880675c4a0c1c4a8884834c
NguyenThu0110 <91600113+nguyenthu753951@users.noreply.github.com>
2023-07-13 09:52:39 +0700

    sửa tên cho router

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,20 +1,23 @@
 import MainScreen from "../components/home/MainScreen.vue";
-import DeliveryPartners from "../components/home/DeliveryPartners.vue";
+import MasterDeliveryPartners from "../components/home/DeliveryPartners.vue";
 import MasterUsers from "../components/home/MasterUsers.vue";
-import DeliveryCustomer from "../components/home/DeliveryCustomer.vue";
+import MasterCustomers from "../components/home/DeliveryCustomer.vue";
 const routes = [
     { path: "/", component: MainScreen, name: "MainScreen" },
     { path: "/home", component: MainScreen, name: "MainScreen" },
     {
-        path: "/delivery-partner",
-        component: DeliveryPartners,
-        name: "DeliveryPartners",
+        path: "/master-delivery-partners",
+        component: MasterDeliveryPartners,
+        name: "MasterDeliveryPartners",
     },
-    { path: "/master-users", component: MasterUsers, name: "MasterUsers" },
     {
-        path: "/delivery-customer",
-        component: DeliveryCustomer,
-        name: "DeliveryCustomer",
+        path: "/master-users",
+        component: MasterUsers,
+        name: "MasterUsers" },
+    {
+        path: "/master-customers",
+        component: MasterCustomers,
+        name: "MasterCustomers",
     },
 ];
 
