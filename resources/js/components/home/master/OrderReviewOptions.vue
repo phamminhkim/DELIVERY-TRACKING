525522d58e529a34a6d1f5cd5a82aa1218f525a7
thinhtran1110 <thinh.tran1110@hcmut.edu.vn>
2023-07-19 13:02:56 +0700

    thêm trang OrderReviewOptions

---
+++ resources/js/components/home/master/OrderReviewOptions.vue
@@ -0,0 +1,58 @@
+<template>
+    <CrudPage :structure="page_structure">
+
+    </CrudPage>
+</template>
+
+<script>
+import Vue from 'vue';
+import CrudPage from '../general/crudform/CrudPage.vue';
+export default {
+    name: 'OrderReviewOptions',
+    components: {
+        Vue,
+        CrudPage
+    },
+    data() {
+        return {
+            page_structure: {}
+        }
+    },
+    created() {
+        this.page_structure = {
+            header: {
+                title: 'OrderReviewOptions',
+                title_icon: 'fas fa-truck',
+            },
+            table: {
+                table_fields: [
+                    {
+                        key: "name",
+                        label: "Tên",
+                        sortable: true,
+                        class: "text-nowrap",
+                    },
+                ],
+                table_cells: [
+
+                ]
+            },
+            form: {
+                unique_name: "order-review-options",
+                form_name: "order review options",
+                form_fields: [
+                    {
+                        label: "Tên",
+                        placeholder: "Nhập tên ..",
+                        key: "name",
+                        type: "text", //html input type
+                        required: true,
+                    },
+                ]
+            },
+            api_url: '/api/master/order-review-options'
+        }
+    }
+
+}
+</script>
