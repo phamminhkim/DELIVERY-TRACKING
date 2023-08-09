b56c26c46b325102d3b25f75f2bcc040254570e0
thinhtran1110 <thinh.tran1110@hcmut.edu.vn>
2023-08-09 10:49:03 +0700

    thêm password vào form tạo user

--- resources/js/components/home/master/Users.vue
+++ resources/js/components/home/master/Users.vue
@@ -1,95 +1,102 @@
 <template>
-    <CrudPage :structure="page_structure">
-        <template #cell(active)="data">
-            <span class="badge bg-success" v-if="data.item.active == 1">Đang hoạt động</span>
-            <span class="badge bg-warning" v-if="data.item.active == 0">Ngưng hoạt động</span>
-        </template>
-    </CrudPage>
+	<CrudPage :structure="page_structure">
+		<template #cell(active)="data">
+			<span class="badge bg-success" v-if="data.item.active == 1">Đang hoạt động</span>
+			<span class="badge bg-warning" v-if="data.item.active == 0">Ngưng hoạt động</span>
+		</template>
+	</CrudPage>
 </template>
 
 <script>
-import Vue from "vue";
-import CrudPage from "../general/crudform/CrudPage.vue";
-export default {
-    name: 'Users',
-    components: {
-        Vue,
-        CrudPage,
-    },
-    data() {
-        return {
-            page_structure: {}
-        };
-    },
-    created() {
-        this.page_structure = {
-            header: {
-                title: 'Người dùng',
-                title_icon: 'fas fa-truck'
-            },
-            table: {
-                table_fields: [
-                    {
-                        key: "name",
-                        label: "Tên người dùng",
-                        sortable: true,
-                        class: "text-nowrap ",
-                    },
-                    {
-                        key: "email",
-                        label: "Email",
-                        sortable: true,
-                        class: "text-nowrap  ",
-                    },
-                    {
-                        key: "phone_number",
-                        label: "Số điện thoại",
-                        sortable: true,
-                        class: "text-nowrap",
-                    },
-                    {
-                        key: "active",
-                        label: "Khả dụng",
-                        sortable: true,
-                        class: "text-nowrap text-center",
-                    },
-                ],
-                table_cells: [
-                    {
-                        target_key: 'active',
-                        type: 'template'
-                    },
-                ]
-            },
-            form: {
-                unique_name: "users",
-                form_name: "người dùng",
-                form_fields: [
-                    {
-                        label: "Tên người dùng",
-                        placeholder: "Nhập tên người dùng..",
-                        key: "name",
-                        type: "text", //html input type
-                        required: true,
-                    },
-                    {
-                        label: "Email",
-                        placeholder: "Nhập email..",
-                        key: 'email',
-                        type: 'email',
-                        required: false
-                    },
-                    {
-                        label: "Số điện thoại",
-                        placeholder: "Nhập số điện thoại..",
-                        key: "phone_number",
-                        type: "number",
-                        required: false
-                    }
-                ]
-            },
-            api_url: 'api/master/users'
-        };
-    },
-};
+	import Vue from 'vue';
+	import CrudPage from '../general/crudform/CrudPage.vue';
+	export default {
+		name: 'Users',
+		components: {
+			Vue,
+			CrudPage,
+		},
+		data() {
+			return {
+				page_structure: {},
+			};
+		},
+		created() {
+			this.page_structure = {
+				header: {
+					title: 'Người dùng',
+					title_icon: 'fas fa-truck',
+				},
+				table: {
+					table_fields: [
+						{
+							key: 'name',
+							label: 'Tên người dùng',
+							sortable: true,
+							class: 'text-nowrap ',
+						},
+						{
+							key: 'email',
+							label: 'Email',
+							sortable: true,
+							class: 'text-nowrap  ',
+						},
+						{
+							key: 'phone_number',
+							label: 'Số điện thoại',
+							sortable: true,
+							class: 'text-nowrap',
+						},
+						{
+							key: 'active',
+							label: 'Khả dụng',
+							sortable: true,
+							class: 'text-nowrap text-center',
+						},
+					],
+					table_cells: [
+						{
+							target_key: 'active',
+							type: 'template',
+						},
+					],
+				},
+				form: {
+					unique_name: 'users',
+					form_name: 'người dùng',
+					form_fields: [
+						{
+							label: 'Tên người dùng',
+							placeholder: 'Nhập tên người dùng..',
+							key: 'name',
+							type: 'text', //html input type
+							required: true,
+						},
+						{
+							label: 'Email',
+							placeholder: 'Nhập email..',
+							key: 'email',
+							type: 'email',
+							required: false,
+						},
+						{
+							label: 'Password',
+							placeholder: 'Nhập mật khẩu..',
+							key: 'password',
+							type: 'password',
+							required: true,
+						},
+						{
+							label: 'Số điện thoại',
+							placeholder: 'Nhập số điện thoại..',
+							key: 'phone_number',
+							type: 'number',
+							required: false,
+						},
+					],
+				},
+				api_url: 'api/master/users',
+			};
+		},
+	};
 </script>
