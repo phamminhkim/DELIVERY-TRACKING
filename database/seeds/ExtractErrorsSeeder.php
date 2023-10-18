7a8ab8bba4885563a8cfc7a6c9526c2b6d14a820
NguyenThu0110 <91600113+nguyenthu753951@users.noreply.github.com>
2023-10-18 16:56:00 +0700

    thêm ExtractErrorsSeeder vào db:seed

--- database/seeds/ExtractErrorsSeeder.php
+++ database/seeds/ExtractErrorsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Business\ExtractError;
+
 
 class ExtractErrorsSeeder extends Seeder
 {
@@ -11,6 +13,31 @@ class ExtractErrorsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $extract_errors = [
+            [
+                'code' => '10',
+                'name' => 'EXTRACT_ERROR',
+            ],
+            [
+                'code' => '20',
+                'name' => 'CONVERT_ERROR',
+            ],
+            [
+                'code' => '30',
+                'name' => 'RESTRUCTURE_ERROR',
+            ],
+        ];
+
+        foreach ($extract_errors as $extract_error) {
+            $existing_error = ExtractError::where('code', $extract_error['code'])->first();
+
+            if ($existing_error) {
+                $existing_error->name = $extract_error['name'];
+                $existing_error->save();
+            } else {
+                ExtractError::create($extract_error);
+            }
+        }
+        $this->command->info('ExtractErrorsSeeder has been completed!');
     }
 }
