8c1821512634dd6679e6b483c1e9e65c200ba045
trieubq <trieu.bq@thienlongvn.com>
2023-12-18 10:13:09 +0700

    Thêm thuộc tính require cho các field để mapping array
    Row nào không thỏa điều kiện sẽ skip

--- app/Services/Implementations/Restructurers/IndexArrayMappingRestructure.php
+++ app/Services/Implementations/Restructurers/IndexArrayMappingRestructure.php
@@ -12,11 +12,26 @@ class IndexArrayMappingRestructure implements DataRestructureInterface
         $collection = collect([]);
         foreach ($data as $match) {
             $output = [];
-            foreach ($structure as $key => $index) {
-                $output[$key] = $match[$index];
+            foreach ($structure as $key => $value_item) {
+                $output[$key] = $match[$value_item['value']];
             }
-            $collection->push($output);
+            // Check trường bắt buộc mà không có giá trị thì skip row
+            if ($this->isValidArrayValue($output, $structure)) {
+                $collection->push($output);
+            }
         }
         return $collection->toArray();
+    }
+
+    public function isValidArrayValue($datas, $structure)
+    {
+        foreach ($structure as $key => $data) {
+            if ($data['require']) {
+                if (!$datas[$key]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
