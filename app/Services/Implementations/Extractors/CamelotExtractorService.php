931b81fb03abfef33e32a910ce109aae9aebb619
trieubq <trieu.bq@thienlongvn.com>
2023-12-20 13:25:36 +0700

    Sử dụng chỉ định number page để xét trường hợp all pages

--- app/Services/Implementations/Extractors/CamelotExtractorService.php
+++ app/Services/Implementations/Extractors/CamelotExtractorService.php
@@ -11,24 +11,14 @@ class CamelotExtractorService implements DataExtractorInterface
 {
     public function extract($file_path, $options)
     {
+        $file_path = "\"" . $file_path . "\"";
+        $instance = new Camelot($file_path, $options['flavor']);
+
+        // Nếu để mặc định thì Camelot chỉ lấy 1 page đầu
         if ($options['is_merge_pages']) {
-            $pdf = new Fpdi();
-            $pageCount = $pdf->setSourceFile($file_path);
-            $size = $pdf->getTemplateSize($pdf->importPage(1));
-            $pdf->AddPage('P', [$size['width'], $size['height'] * $pageCount]);
-            for ($i = 1; $i <= $pageCount; $i++) {
-                $tpl = $pdf->importPage($i);
-                $pdf->useTemplate($tpl, 0, $size['height'] * ($i - 1));
-            }
-            $pdf->Output('F', $file_path);
+            $instance->pages('1-end');
         }
 
-        $file_path = "\"" . $file_path . "\"";
-
-        $instance = new Camelot($file_path, $options['flavor']);
-        //$instance->inRegions(
-        //    Areas::from($xTopLeft, $yTopLeft, $xBottomRight, $yBottomRight)
-        //);
         $table = $instance->extract();
         return $table;
     }
