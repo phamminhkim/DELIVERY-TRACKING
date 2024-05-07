980ecfc26540a36c01c20fe3e972feabeb9180fa
NguyenThu0110 <91600113+nguyenthu753951@users.noreply.github.com>
2024-05-07 14:39:26 +0700

    sửa lỗi báo message combo

---
+++ database/migrations/2024_05_07_104439_rename_is_deleted_to_is_active.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameIsDeletedToIsActive extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('material_combos', function (Blueprint $table) {
+            // Rename the column
+            $table->renameColumn('is_deleted', 'is_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('material_combos', function (Blueprint $table) {
+            // Rename the column back to its original name
+            $table->renameColumn('is_active', 'is_deleted');
+        });
+    }
+}
