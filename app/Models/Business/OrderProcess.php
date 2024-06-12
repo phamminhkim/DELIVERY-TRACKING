58c193d3a1e1fbadbe70062150ba6c654f9cb679
trieubq <trieu.bq@thienlongvn.com>
2024-06-12 17:46:57 +0700

    Thêm trạng thái đang đồng bộ SAP
    Tránh lưu data lúc đang sync

--- app/Models/Business/OrderProcess.php
+++ app/Models/Business/OrderProcess.php
@@ -32,7 +32,7 @@ class OrderProcess extends Model
     {
         // Lấy data chưa đồng bộ SAP
         return $this->hasMany(SoDataItem::class)->whereHas('so_header', function ($query) {
-            $query->whereNull('so_uid')->where('is_sync_sap', 0);
+            $query->whereNull('so_uid')->where('is_syncing_sap', 0);
         });
     }
     public function is_sync_so_data($sap_so_number, $promotive_name)
@@ -41,7 +41,7 @@ class OrderProcess extends Model
         $sync_data =  $this->hasMany(SoDataItem::class)->where('promotive_name', $promotive_name)
         ->whereHas('so_header', function ($query) use ($sap_so_number) {
             $query->where('sap_so_number', $sap_so_number)->where(function ($qu) {
-                $qu->whereNotNull('so_uid')->orWhere('is_sync_sap', 1);
+                $qu->whereNotNull('so_uid')->orWhere('is_syncing_sap', 1);
             });
         });
         return $sync_data->count() > 0 ? true : false;
@@ -49,12 +49,12 @@ class OrderProcess extends Model
     public function not_sync_so_headers()
     {
         // Lấy data chưa đồng bộ SAP
-        return $this->hasMany(SoHeader::class)->whereNull('so_uid')->where('is_sync_sap', 0);
+        return $this->hasMany(SoHeader::class)->whereNull('so_uid')->where('is_syncing_sap', 0);
     }
     public function sync_so_headers()
     {
         // Lấy data đã hoặc đang đồng bộ SAP
-        return $this->hasMany(SoHeader::class)->whereNotNull('so_uid')->orWhere('is_sync_sap', 1);
+        return $this->hasMany(SoHeader::class)->whereNotNull('so_uid')->orWhere('is_syncing_sap', 1);
     }
     public function created_by()
     {
