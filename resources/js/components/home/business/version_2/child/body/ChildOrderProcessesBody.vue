a0ab3aeae225fe3810f6cf6c456f0a05b98ae46f
NguyenThu0110 <nguyenthu753951@gmail.com>
2024-12-10 16:07:15 +0700

    Update ChildOrderProcessesBody.vue

--- resources/js/components/home/business/version_2/child/body/ChildOrderProcessesBody.vue
+++ resources/js/components/home/business/version_2/child/body/ChildOrderProcessesBody.vue
@@ -643,16 +643,16 @@ export default {
                     }
                     if (data.promotion_category == 'X') {
                         row.getCell('barcode').getElement().style.color = '#007bff';
-                        // row.getCell('barcode').getElement().style.fontWeight = 'bold';
+                        row.getCell('barcode').getElement().style.fontWeight = 'bold';
                         row.getCell('sku_sap_code').getElement().style.color = '#007bff';
-                        // row.getCell('sku_sap_code').getElement().style.fontWeight = 'bold';
+                        row.getCell('sku_sap_code').getElement().style.fontWeight = 'bold';
 
                     }
                     if (data.promotion_bundle == 'X') {
-                        // row.getCell('barcode').getElement().style.fontWeight = 'bold';
+                        row.getCell('barcode').getElement().style.fontWeight = 'bold';
                         row.getCell('barcode').getElement().style.color = '#b40fcb';
                         row.getCell('sku_sap_code').getElement().style.color = '#b40fcb';
-                        // row.getCell('sku_sap_code').getElement().style.fontWeight = 'bold';
+                        row.getCell('sku_sap_code').getElement().style.fontWeight = 'bold';
                     }
                     // hiển thị dữ liệu của cột 'sap_so_number' và 'promotive' trong cùng một ô
                     const cell_sap_so_number = row.getCell('sap_so_number');
